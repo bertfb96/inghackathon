6470c39c9ea3bac92bac84bcb1a78d9110eca886
Mehmet Seven <mehmetseven0@gmail.com>
2016-11-05 18:20:59 +0300

    test

--- api/app.js
+++ api/app.js
@@ -21,7 +21,7 @@ mongoose.connect('mongodb://localhost/inghackathon');
 
 //Socket.io
 var io = require('socket.io').listen(app.listen(1000));
-io.set( 'origins', '*localhost:8100' );
+io.set( 'origins', '*' );
 
 io.sockets.on('connection', function (socket) {
     socket.on('message', function (data) {
